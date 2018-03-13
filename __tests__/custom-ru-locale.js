683473eef549c285d7b86d194318424805508f2f
NeZeD [Mac Air] <nezed@ya.ru>
2018-03-13 17:47:26 +0300

    Fix durations displaying in RU locale

--- __tests__/custom-ru-locale.js
+++ __tests__/custom-ru-locale.js
@@ -1,6 +1,8 @@
 const moment = require('../src')
 
 describe('Custom RU locale', () => {
+  moment.locale('ru')
+
   test('Correctly formats short months', () => {
     expect(
       moment('2020-01-01').format('DD MMM')
@@ -9,5 +11,23 @@ describe('Custom RU locale', () => {
     expect(
       moment('2020-07-01').format('DD MMM')
     ).toBe('01 июл')
+  })
+
+  test('Strict formatting of durations', () => {
+    expect(
+      moment.duration(1, 'm').humanize()
+    ).toBe('1 минута')
+
+    expect(
+      moment.duration(59, 'm').humanize()
+    ).toBe('59 минут')
+
+    expect(
+      moment.duration(1, 'h').humanize()
+    ).toBe('1 час')
+
+    expect(
+      moment.duration(23, 'h').humanize()
+    ).toBe('23 часа')
   })
 })
--- src/index.js
+++ src/index.js
@@ -8,6 +8,18 @@ moment.updateLocale('ru', {
     format: monthsNames,
     standalone: monthsNames,
   },
+  relativeTime: {
+    m: function (number, withoutSuffix, key) {
+      return '1 ' + (withoutSuffix ? 'минута' : 'минуту')
+    },
+    h: '1 час',
+    d: '1 день',
+    M: '1 месяц',
+    y: '1 год',
+  }
 })
+
+moment.relativeTimeThreshold('m', 60); // Keep minutes "as-is" instead of rounding 45 minutes as "an hour"
+moment.relativeTimeThreshold('h', 24); // Keep hours "as-is" instead of rounding 23 hours as "a day"
 
 module.exports = moment
